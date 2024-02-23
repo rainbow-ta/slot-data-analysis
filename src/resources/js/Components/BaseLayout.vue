c2500debd6014e00e7d9ee9e92f88c710802fd37
kamikitakamikita <ebinakota.h@gmail.com>
2024-02-23 13:52:27 +0900

    footerを作成

--- src/resources/js/Components/BaseLayout.vue
+++ src/resources/js/Components/BaseLayout.vue
@@ -21,7 +21,10 @@ import { Link } from '@inertiajs/vue3'
       <slot />
     </main>
     <footer>
-      <slot name="footer" />
+      <div class="w-full mx-auto p-4 md:py-8">
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">&copy; 2023 slot-data-analysis.</span>
+      </div>
     </footer>
   </div>
 </template>
