6a9f12e6c2747d6f104141389988bb9213ef1c62
kamikitakamikita <ebinakota.h@gmail.com>
2024-03-08 17:21:13 +0900

    クエリの不具合を修正

--- src/app/Services/EventHallDataService.php
+++ src/app/Services/EventHallDataService.php
@@ -14,6 +14,7 @@ class EventHallDataService
             ->whereExists(function ($query) {
                 $query->select(DB::raw(1))
                     ->from('hall_events')
+                    ->whereRaw('hall_events.hall_id = hall_data.hall_id')
                     ->whereRaw('hall_events.date = hall_data.date');
             })
             ->get();
