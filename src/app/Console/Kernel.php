e5c46c5d47ab8f321b48ff1929ef961599b4a87c
kamikitakamikita <ebinakota.h@gmail.com>
2024-07-20 22:08:30 +0900

    cronを設定

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('command:scrape-anaslot')->dailyAt('03:00');
     }
 
     /**
